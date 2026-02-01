8eef6d036f5a6b78f4733fe0e09fdb94882faf65
gowtham873 <pediredlagowtham02@gmail.com>
2026-02-01 08:24:04 +0000

    Change instance type to t3.micro for free tier

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 
-  region = "eu-west-1"
+  region = "eu-north-1"
 
 }
 
@@ -10,7 +10,7 @@ resource "aws_instance" "gitops_ec2" {
 
   ami           = "ami-0d940f23d527c3ab1"
 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
 
 
